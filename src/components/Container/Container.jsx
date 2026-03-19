afbaace71ee149b855bd6d996bdbb6094c39127c
viktoria kolosova <vikakolosova04@gmail.com>
2026-03-19 14:03:00 +0300

    apply className prop in Container component

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import './Container.css';
 
 export const Container = ({ children, className = '' }) => {
-  return <div className="container">{children}</div>;
+  return <div className={`container ${className}`.trim()}>{children}</div>;
 };
